3346212475392a12c383ed9d4cdb2cfe7a6367bf
isubfox <samuel.liska@gmail.com>
2026-01-18 22:05:36 +0100

    feature: add profile component

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile {
-
+  name: string = 'Samuel Liska';
+  role: string = 'Software developer';
+  location: string = 'Eindhoven, Nederland';
+  linkedinUrl: string = 'https://www.linkedin.com/in/samuel-liska-19810149/';
+  githubUrl: string = 'https://github.com/Biosaiber';
 }
